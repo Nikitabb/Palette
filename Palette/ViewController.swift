893f74b6fe83f74f584b92e4d24f4c3a932846e6
Nikita <116796802+Nikitabb@users.noreply.github.com>
2023-03-26 19:00:31 +0300

    added color change methods

--- Palette/ViewController.swift
+++ Palette/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var paletteView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        paletteView.layer.cornerRadius = 10
     }
 
-
+    @IBAction func redSliderAction() {
+        redLabel.text = redSlider.value.formatted(.number.precision(.fractionLength(2)))
+        changeColor(r: CGFloat(redSlider.value))
+    }
+    @IBAction func greenSliderAction() {
+        greenLabel.text = greenSlider.value.formatted(.number.precision(.fractionLength(2)))
+        changeColor(g: CGFloat(greenSlider.value))
+    }
+    @IBAction func blueSliderAction() {
+        blueLabel.text = blueSlider.value.formatted(.number.precision(.fractionLength(2)))
+        changeColor(b: CGFloat(blueSlider.value))
+    }
+    
+    private func changeColor(r: CGFloat? = nil, g: CGFloat? = nil, b: CGFloat? = nil) {
+        var redPalette: CGFloat = 0
+        var greenPalette: CGFloat = 0
+        var bluePalette: CGFloat = 0
+        
+        _ = paletteView.backgroundColor?.getRed(&redPalette, green: &greenPalette, blue: &bluePalette, alpha: nil)
+        
+        let red = r ?? redPalette
+        let green = g ?? greenPalette
+        let blue = b ?? bluePalette
+        
+        let newPalleteColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+        paletteView.backgroundColor = newPalleteColor
+    }
 }
 
